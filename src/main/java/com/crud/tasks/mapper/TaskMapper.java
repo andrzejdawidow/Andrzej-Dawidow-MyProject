696d21648cba9a9fb14ab6dedede8fb7418c16a9
AndrzejDawidow <andrzejdawidow@gmail.com>
2018-03-05 22:00:55 +0100

    initial commit

--- src/main/java/com/crud/tasks/mapper/TaskMapper.java
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -15,6 +15,13 @@ public class TaskMapper {
                 taskDto.getContent());
     }
 
+    public TaskDto mapToTaskDto(final Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getContent());
+    }
+
     public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {
         return taskList.stream()
                 .map(t -> new TaskDto(t.getId(), t.getTitle(), t.getContent()))
